731852f6d8bed46149361d97f279d5504578210a
Chetan Bohra <chetan.bohra@flexiele.com>
2020-02-19 19:06:44 +0530

    API working

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
-var http = require('http');
+var express = require('express');
+var {Client} = require('pg');
+
+var app = express();
+
 const port = process.env.PORT || 3000;
+const conStr = process.env.DATABASE_URL ||"postgres://kckhzihgigctnf"+":"
+		+"138cfc5a9c1476cb711e8d49649d50f3a313b966266ffe6644475df8c027f7d2@ec2-3-213-192-58.compute-1.amazonaws.com"+":"
+		+"5432/df5lb9h3l32bm9";
 
-const server = http.createServer(function(req,res){
-	res.statusCode = 200;
-	res.setHeader('Content-Type','text/html');
-	res.end('Hello');
+const client = new Client({
+	connectionString: conStr,
+	ssl:true,
+	rejectUnauthorized: false
 });
 
-server.listen(port,()=>{
-	console.log('Listening on port '+port);
+app.get('/',function(req,res){
+	//res.send("Hello..!");
+	selectTable(res);
+}).listen(6969);
+console.log('Listening on port 6969');
+
+client.connect(function(err){
+	if(err)	throw err;
+	console.log('Connected to database');
 });
+
+const selectTable = function(res){
+	console.log("Check");
+		let selectQuery = "select * from Users;";
+		//console.log(selectQuery);
+		client.query(selectQuery,function(err,result){
+			if(err)	throw err;
+			//console.log(result.rows);
+			res.send(JSON.stringify(result.rows));
+			//res.end(JSON.stringify(result.rows));
+		});
+}
